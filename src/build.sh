96960cb0f181c57701672ca3a33524b47eae1ac7
Peter Welinder <peter.welinder@gmail.com>
2010-04-28 23:17:39 -0700

    added python packages (rope, pymacs, pyflakes) and installer script

---
+++ src/build.sh
@@ -0,0 +1,29 @@
+# Stolen from Brandon Craig Rhodes (THANKS!!!):
+# http://rhodesmill.org/brandon/2009/emacs-python-virtualenv/
+
+# Change to the directory containing this script.
+cd $(dirname "$0")
+
+# Clean up directories left over from previous run.
+DIRS=(Pymacs-0.23 rope-0.9.2 ropemacs-0.6c2 pyflakes-0.4.0)
+rm -rf ../usr ${DIRS[@]}
+
+# Create the virtualenv.
+python2.6 virtualenv.py ../usr
+source ../usr/bin/activate
+
+# Unpack the four Python packages that Emacs needs.
+tar xvfz Pymacs.tar.gz
+tar xvfz rope-0.9.2.tar.gz
+tar xvfz ropemacs-0.6c2.tar.gz
+tar xvfz pyflakes-0.4.0.tar.gz
+
+# Install them in "~/.emacs/usr" (the "python2.5" that
+# gets called here is the one in the virtualenv, thanks
+# to my having sourced its "activate" script up above).
+for D in ${DIRS[@]}
+do cd $D; python2.5 setup.py install; cd ..; done
+
+# Make fresh pymacs.el symlink from ~/.emacs
+rm -f ../vendor/pymacs.el
+ln -s src/Pymacs-0.23/pymacs.el ../vendor/pymacs.el
